c155b383e7e3b2664e839c5648f43c94003e89f2
Utokato <utokato@outlook.com>
2019-09-10 17:31:18 +0800

    modify the file to add some info step by step to dev webflux

--- webflux01/src/main/java/cn/utokato/webflux01/Webflux01Application.java
+++ webflux01/src/main/java/cn/utokato/webflux01/Webflux01Application.java
@@ -1,10 +1,27 @@
 package cn.utokato.webflux01;
 
+import cn.utokato.webflux01.domain.User;
+import cn.utokato.webflux01.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 /**
+ * 开发 WebFlux 的基本操作：
+ * 1. 安装 MongoDB 数据库
+ * -    使用 Docker 直接安装
+ * 2. 引入MongoDB数据库依赖
+ * -    spring-boot-starter-data-mongodb-reactive
+ * 3. 开启数据库相关的注解
+ * -    {@link EnableReactiveMongoRepositories}
+ * 4. 定义对象
+ * -    {@link User}
+ * 5. 使用 JPA 操作 MongoDB
+ * -    {@link UserRepository}
+ * 6. 数据库的配置
+ * -    见 application.yml
+ * 7. CRUD 编程，详见代码
+ *
  * @author lma
  * @date 2019/09/03
  */

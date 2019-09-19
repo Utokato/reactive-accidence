9312771c249a0a348b9340f5bf2fb5d9fa292b94
Utokato <utokato@outlook.com>
2019-09-19 17:08:12 +0800

    modify a file about keyword this in java

--- 01-lambda-accidence/src/main/java/cn/utokato/lambda/ThisDemo.java
+++ 01-lambda-accidence/src/main/java/cn/utokato/lambda/ThisDemo.java
@@ -5,10 +5,15 @@ package cn.utokato.lambda;
  *
  * @author lma
  * @date 2019/09/12
+ * @modifyDate 2019/09/19
  */
 public class ThisDemo {
     private String name = "JustTheName";
 
+    /**
+     * 对于匿名内部类而言，最终会生成一个新的类文件
+     * 由于this总是指向当前类，所以指向了新类的作用空间
+     */
     public void testAnonymousInnerClass() {
         new Thread(new Runnable() {
             String name = "TheInnerName";
@@ -21,6 +26,10 @@ public class ThisDemo {
 
     }
 
+    /**
+     * 对于Lambda而言，最后会在本类中生成一个方法
+     * 由于this总是指向当前类，所以指向了外层类的作用空间
+     */
     public void testLambda() {
         new Thread(() -> {
             String name = "TheLambdaName"; // 不能通过 this.name 访问到该变量
@@ -33,6 +42,5 @@ public class ThisDemo {
         ThisDemo demo = new ThisDemo();
         demo.testAnonymousInnerClass(); // TheInnerName
         demo.testLambda(); // JustTheName
-
     }
 }
